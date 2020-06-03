887efc1be266d69d88c49f8844459a712759807c
Vladimir Beliashov <v.belyashov@eastwind.ru>
2020-06-04 02:37:58 +0500

    simple version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Todo todo todoo todotodotodo todo todoooo tododododo')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Do nothing</li>
+      <li>Sleep</li>
+    </ul>
+  );
+}
+
+const Header = () => {
+  return <h1>WOODOO list</h1>;
+}
+
+const Search = () => {
+  return <input placeholder="search"/>;
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Search />
+      <TodoList />
+    </div>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
