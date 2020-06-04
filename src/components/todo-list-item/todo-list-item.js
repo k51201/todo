b732874dfb60bf237e6f4ca1fff369c74753dd14
Vladimir Beliashov <v.belyashov@eastwind.ru>
2020-06-05 01:05:35 +0500

    made list item work with state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,29 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './todo-list-item.css'
 
-const TodoListItem = ({ label, important = false }) => {
-    const itemStyle = {
-        color: important ? 'tomato' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
+export default class TodoListItem extends Component {
+    state = {
+        done: false,
+        important: false
     }
 
-    return (
-        <span className="todo-list-item">
-            <span className="todo-list-item-label"
-                style={itemStyle}>
-                {label}
-            </span>
-            <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-                <i className="fa fa-exclamation" />
-            </button>
-            <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-                <i className="fa fa-trash-o" />
-            </button>
-        </span>
-    )
-}
+    onLabelClick = () => {
+        this.setState({ done: !this.state.done })
+    }
 
-export default TodoListItem
+    onClickImportant = () => {
+        this.setState({ important: !this.state.important })
+    }
+
+    render() {
+        const { label } = this.props
+        const { done, important } = this.state
+
+        const itemClasses = 'todo-list-item' +
+            (done ? ' done' : '') +
+            (important ? ' important' : '')
+
+        return (
+            <span className={itemClasses}>
+                <span className="todo-list-item-label"
+                    onClick={this.onLabelClick}>
+                    {label}
+                </span>
+                <button type="button"
+                    className="btn btn-outline-success btn-sm float-right"
+                    onClick={this.onClickImportant}>
+                    <i className="fa fa-exclamation" />
+                </button>
+                <button type="button"
+                    className="btn btn-outline-danger btn-sm float-right">
+                    <i className="fa fa-trash-o" />
+                </button>
+            </span>
+        )
+    }
+}
