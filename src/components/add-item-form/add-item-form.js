4474390fbbdcb2c6aac0c0265c309e7c74be130e
Vladimir Beliashov <k51201@vampa.ru>
2020-06-05 21:46:13 +0500

    finished add item form

--- src/components/add-item-form/add-item-form.js
+++ src/components/add-item-form/add-item-form.js
@@ -3,16 +3,34 @@ import React, { Component } from 'react'
 import './add-item-form.css'
 
 export default class AddItemForm extends Component {
-    render() {
+    state = {
+        label: ''
+    }
+
+    onLabelChange = (e) => {
+        this.setState({ label: e.target.value })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
         const { onAddItem } = this.props
+        onAddItem(this.state.label)
+        this.setState({ label: '' })
+    }
+
+    render() {
         return (
-            <div className="add-item-form">
-                
-                <button className="btn btn-outline-secondary"
-                    onClick={() => onAddItem('lol')}>
+            <form className="add-item-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text"
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder="Co ještě máte udělat"
+                    value={this.state.label}
+                />
+                <button className="btn btn-outline-secondary">
                     Přidat
                 </button>
-            </div>
+            </form>
         )
     }
 }
